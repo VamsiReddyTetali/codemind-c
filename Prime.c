5fe17c9f07fa826d53ba0f464b1c0bb166617f81
VamsiReddyTetali <119839763+VamsiReddyTetali@users.noreply.github.com>
2022-12-22 20:14:13 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     if(n==0&&n==1)c=1;     for(i=2;i<=n;i++)     {         if(n%i==0)c=1;         break;     }     if(c==0)printf("Prime");     else printf("Not Prime"); }

---
+++ Prime.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    if(n==0&&n==1)c=1;
+    for(i=2;i<=n;i++)
+    {
+        if(n%i==0)c=1;
+        break;
+    }
+    if(c==0)printf("Prime");
+    else printf("Not Prime");
+}

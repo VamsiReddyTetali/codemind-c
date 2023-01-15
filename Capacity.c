6df40ae990a2b633fae27d447c452caef474489f
VamsiReddyTetali <119839763+VamsiReddyTetali@users.noreply.github.com>
2023-01-15 08:50:29 +0530

    #include<stdio.h> int main() {     int s,t,b,k,kb;     scanf("%d%d%d",&s,&t,&b);     k=2*s*t*b*512;     kb=k/1024;     printf("%d KB",kb); }

--- Capacity.c
+++ Capacity.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int s,t,b,c,k;
+    int s,t,b,k,kb;
     scanf("%d%d%d",&s,&t,&b);
-    c=2*s*t*b*512;
-    k=c/1024;
-    printf("%dkb",k);
+    k=2*s*t*b*512;
+    kb=k/1024;
+    printf("%d KB",kb);
 }

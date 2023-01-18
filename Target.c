0b47ec41da9739e66d1d3b2da03e0b86bbefb986
VamsiReddyTetali <119839763+VamsiReddyTetali@users.noreply.github.com>
2023-01-18 11:44:54 +0530

    #include<stdio.h> int main() {     int a,b,c,d;     scanf("%d%d%d%d",&a,&b,&c,&d);     if(a>9&&b>9&&c>9&&d>9)printf("YES");     else printf("NO"); }

--- Target.c
+++ Target.c
@@ -3,6 +3,6 @@ int main()
 {
     int a,b,c,d;
     scanf("%d%d%d%d",&a,&b,&c,&d);
-    if(a>=10&&b>=10&&c>=10&&d>=10)printf("YES");
+    if(a>9&&b>9&&c>9&&d>9)printf("YES");
     else printf("NO");
 }
